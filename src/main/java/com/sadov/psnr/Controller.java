23dfc81769db0dbd75b08a52377773ef9a7b4319
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-12-17 23:58:18 +0300

    GUI

--- src/main/java/com/sadov/psnr/Controller.java
+++ src/main/java/com/sadov/psnr/Controller.java
@@ -1,9 +1,17 @@
 package com.sadov.psnr;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -20,14 +28,40 @@ public class Controller {
     private Button origImageButton;
 
     @FXML
+    private ImageView origImgView;
+
+    @FXML
+    private ImageView modImgView;
+
+    @FXML
     void initialize() {
         origImageButton.setOnAction(actionEvent -> {
-
+            FileChooser fileChooser = fileChooser();
+            Stage stage = (Stage) origImageButton.getScene().getWindow();
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                Image image = new Image(file.toURI().toString());
+                origImgView.setImage(image);
+            }
         });
 
         modernImageButton.setOnAction(actionEvent -> {
-
+            FileChooser fileChooser = fileChooser();
+            Stage stage = (Stage) modernImageButton.getScene().getWindow();
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                Image image = new Image(file.toURI().toString());
+                modImgView.setImage(image);
+            }
         });
+    }
+
+    private FileChooser fileChooser() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open a file");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+
+        return  fileChooser;
     }
 
 }
