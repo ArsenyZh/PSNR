0aa54f390d1534bf5bb1c5ef17d740bbfab4dd50
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-12-24 00:34:48 +0300

    fin

--- src/main/java/com/sadov/psnr/Controller.java
+++ src/main/java/com/sadov/psnr/Controller.java
@@ -2,6 +2,7 @@ package com.sadov.psnr;
 
 import java.io.File;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -25,6 +26,8 @@ public class Controller {
     private int startY;
     private int width;
     private int height;
+    Image image1;
+    Image image2;
 
 
     @FXML
@@ -66,8 +69,8 @@ public class Controller {
             Stage stage = (Stage) origImageButton.getScene().getWindow();
             File file = fileChooser.showOpenDialog(stage);
             if (file != null && file.exists()) {
-                Image image = new Image(file.toURI().toString());
-                origImgView.setImage(image);
+                image1 = new Image(file.toURI().toString());
+                origImgView.setImage(image1);
 
 //                getPixelPropertiesZone(image);
             }
@@ -78,8 +81,8 @@ public class Controller {
             Stage stage = (Stage) modernImageButton.getScene().getWindow();
             File file = fileChooser.showOpenDialog(stage);
             if (file != null && file.exists()) {
-                Image image = new Image(file.toURI().toString());
-                modImgView.setImage(image);
+                image2 = new Image(file.toURI().toString());
+                modImgView.setImage(image2);
 
 //                getPixelProperties(image);
             }
@@ -110,7 +113,18 @@ public class Controller {
             } catch (NumberFormatException e) {
                 System.out.println("Пожалуйста, введите числовые значения.");
             }
-            PSNRValue.setText("PSNR: 0%");
+            double psnr = 0;
+
+            if(textFieldY.isVisible()){
+                psnr = getPixelPropertiesZone(image1,image2);
+            }else {
+                psnr = getPixelProperties(image1,image2);
+            }
+
+            DecimalFormat df = new DecimalFormat("#.00");
+            String formattedValue = df.format(psnr);
+
+            PSNRValue.setText("PSNR: " + formattedValue);
         });
     }
 
@@ -122,40 +136,75 @@ public class Controller {
         return  fileChooser;
     }
 
-    private void getPixelProperties(Image image) {
-        WritableImage writableImage = new WritableImage((int) image.getWidth(), (int) image.getHeight());
+    private double getPixelProperties(Image image, Image image1) {
         PixelReader pixelReader = image.getPixelReader();
+        PixelReader pixelReader1 = image1.getPixelReader();
+        height =(int) image.getHeight();
+        width = (int) image1.getWidth();
+
+        double mse = 0.0;
 
         for (int x = 0; x < image.getWidth(); x++) {
             for (int y = 0; y < image.getHeight(); y++) {
-                Color color = pixelReader.getColor(x, y);
-                int red = (int) (color.getRed() * 255);
-                int green = (int) (color.getGreen() * 255);
-                int blue = (int) (color.getBlue() * 255);
-                int brightness = (int) (color.getBrightness() * 255);
+                Color color1 = pixelReader.getColor(x, y);
+                Color color2 = pixelReader1.getColor(x, y);
+                mse+=getMSE(color1,color2);
 
-                System.out.printf("Пиксель (%d, %d): RGB(%d, %d, %d), Яркость: %d%n", x, y, red, green, blue, brightness);
             }
         }
+        return 10 * Math.log10((255 * 255) / mse);
     }
 
-    private void getPixelPropertiesZone(Image image) {
+    private double getPixelPropertiesZone(Image image, Image image1) {
         PixelReader pixelReader = image.getPixelReader();
+        PixelReader pixelReader1 = image1.getPixelReader();
 
         if (startX + width > image.getWidth()) width = (int) image.getWidth() - startX;
         if (startY + height > image.getHeight()) height = (int) image.getHeight() - startY;
 
+        double mse = 0.0;
+
         for (int x = startX; x < startX + width; x++) {
             for (int y = startY; y < startY + height; y++) {
-                Color color = pixelReader.getColor(x, y);
-                int red = (int) (color.getRed() * 255);
-                int green = (int) (color.getGreen() * 255);
-                int blue = (int) (color.getBlue() * 255);
-                int brightness = (int) (color.getBrightness() * 255);
+                Color color1 = pixelReader.getColor(x, y);
+                Color color2 = pixelReader1.getColor(x, y);
+                mse+=getMSE(color1,color2);
 
-                System.out.printf("Пиксель (%d, %d): RGB(%d, %d, %d), Яркость: %d%n", x, y, red, green, blue, brightness);
             }
         }
+        return 10 * Math.log10((255 * 255) / mse);
+
+    }
+    private  double getMSE(Color color1, Color color){
+        double mse = 0.0;
+
+
+
+
+        int originalRed = (int) (color.getRed() * 255);
+        int originalGreen = (int) (color.getGreen() * 255);
+        int originalBlue = (int) (color.getBlue() * 255);
+
+        int modifiedRed = (int) (color1.getRed() * 255);
+        int modifiedGreen = (int) (color1.getGreen() * 255);
+        int modifiedBlue = (int) (color1.getBlue() * 255);
+
+        // Вычисляем разницу для каждого канала
+        double diffRed = originalRed - modifiedRed;
+        double diffGreen = originalGreen - modifiedGreen;
+        double diffBlue = originalBlue - modifiedBlue;
+
+        // Суммируем квадраты разниц
+        mse += (diffRed * diffRed + diffGreen * diffGreen + diffBlue * diffBlue) / 3.0;  // Среднее по канала}
+
+// Вычисляем среднее значение MSE
+        mse /= (width * height);
+
+                // Вычисляем PSNR
+                double psnr = 10 * Math.log10((255 * 255) / mse);
+                return mse;
+
     }
 }
+
 
